2aa806f9dba5954a549ae364b67e14add7411fa8
coderaky <45727674+coderaky@users.noreply.github.com>
2020-07-07 13:00:14 +0530

    intial stateless widget

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: BallPage(),
+      ),
+    );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text('Ask Me Anything '),
+          backgroundColor: Colors.blue.shade900,
+        ),
+      ),
+    );
+  }
+}
